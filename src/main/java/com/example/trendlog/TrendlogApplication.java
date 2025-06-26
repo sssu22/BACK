8e4bba40ea832cca68953e801c476ecf2b30ffc7
sehyunnnn <kkshyun56@gmail.com>
2025-06-26 21:41:49 +0900

    first commit

---
+++ src/main/java/com/example/trendlog/TrendlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.trendlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendlogApplication.class, args);
+	}
+
+}
