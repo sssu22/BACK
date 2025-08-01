3ab4467f0bb422c76a57f47aa5ae1c03d5a675e3
nenini <kyr068600@gmail.com>
2025-08-01 20:18:50 +0900

    feat : 트렌드 목록 CSV 파일 생성

--- src/main/java/com/example/trendlog/service/TrendStatisticsScheduler.java
+++ src/main/java/com/example/trendlog/service/TrendStatisticsScheduler.java
@@ -11,13 +11,20 @@ import java.util.List;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class TrendstatisticsScheduler {
+public class TrendStatisticsScheduler {
     private final TrendRecommendScoreExportService trendRecommendScoreExportService;
+    private final TrendExportService trendExportService;
 
     @Scheduled(cron = "0 0 3 * * *") // 매일 3시
     public void updateRecommendationScores() {
         List<TrendRecommendScoreDto> scores = trendRecommendScoreExportService.getAllTrendScores();
         trendRecommendScoreExportService.exportScoresToCsv(scores);
         log.info("추천 점수 계산 완료. 총 건수: {} " , scores.size());
+    }
+
+    @Scheduled(cron = "0 0 3 * * *") // 매일 3시
+    public void updateTrendCsvFile() {
+        trendExportService.exportAllTrendsToCsv();
+        log.info("전체 트렌드 csv 파일 생성 완료.");
     }
 }
